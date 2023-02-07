46d38d71a19f3ac5d0d983c56fbb8eb6c5fc194d
AndrewJoeBaj <99694169+AndrewJoeBaj@users.noreply.github.com>
2023-02-07 14:48:44 +0100

    tried adding the create and display view and added the ViewModels but an error about the namespace is occuring

---
+++ EnterpriseSynoptic2023/BusinessLogic/ViewModels/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.View_Model
+{
+    class ContactViewModel
+    {
+   
+        public int Id { get; set; }
+
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public byte[] Picture { get; set; }
+    }
+}
---
+++ EnterpriseSynoptic2023/WebApplication1/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public string FirstName { get; set; }
+        [StringLength(255)]
+        public string LastName { get; set; }
+        [StringLength(15)]
+        public string MobileNumber { get; set;}
+
+        public byte[] Picture { get; set; }
+    }
+
+
+
+}
